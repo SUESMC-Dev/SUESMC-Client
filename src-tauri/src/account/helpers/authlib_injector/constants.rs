ddb5ddb8a4cdae058b2b4a94f8a48beb7739c146
a1375625918 <87858091+a1375625918@users.noreply.github.com>
2025-09-07 00:44:52 +0800

    chore: configure client id

--- src-tauri/src/account/helpers/authlib_injector/constants.rs
+++ src-tauri/src/account/helpers/authlib_injector/constants.rs
@@ -1,8 +1,9 @@
 pub static PRESET_AUTH_SERVERS: [&str; 2] = [
-  "https://user.suesmc.ltd/api/yggdrasil",
+  "https://user-dev.suesmc.ltd/api/yggdrasil",
   "https://skin.mualliance.ltd/api/yggdrasil",
 ];
-pub static CLIENT_IDS: [(&str, &str); 2] = [("user.suesmc.ltd", "6"), ("littleskin.cn", "1014")];
+pub static CLIENT_IDS: [(&str, &str); 2] =
+  [("user-dev.suesmc.ltd", "2"), ("littleskin.cn", "1014")];
 pub static SCOPE: &str =
   "openid offline_access Yggdrasil.PlayerProfiles.Select Yggdrasil.Server.Join";
 pub static AUTHLIB_INJECTOR_JAR_NAME: &str = "authlib-injector.jar";
