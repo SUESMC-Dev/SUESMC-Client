43113b0e4a655d09f8d4c2c69e90c6da95b64fa6
a1375625918 <87858091+a1375625918@users.noreply.github.com>
2025-09-14 00:44:19 +0800

    fix: avoid using sjmcl client ids

--- src-tauri/src/account/helpers/authlib_injector/constants.rs
+++ src-tauri/src/account/helpers/authlib_injector/constants.rs
@@ -2,12 +2,7 @@ pub static PRESET_AUTH_SERVERS: [&str; 2] = [
   "https://user.suesmc.ltd/api/yggdrasil",
   "https://skin.mualliance.ltd/api/yggdrasil",
 ];
-pub static CLIENT_IDS: [(&str, &str); 4] = [
-  ("skin.mc.sjtu.cn", "6"),
-  ("skin.mualliance.ltd", "27"),
-  ("littleskin.cn", "1014"),
-  ("user-dev.suesmc.ltd", "3"),
-];
+pub static CLIENT_IDS: [(&str, &str); 1] = [("user.suesmc.ltd", "5")];
 pub static SCOPE: &str =
   "openid offline_access Yggdrasil.PlayerProfiles.Select Yggdrasil.Server.Join";
 pub static AUTHLIB_INJECTOR_JAR_NAME: &str = "authlib-injector.jar";
