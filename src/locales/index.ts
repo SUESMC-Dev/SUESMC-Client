e38be307df12132a025e44f62b32cc4469fdbf18
a1375625918 <87858091+a1375625918@users.noreply.github.com>
2025-09-13 11:51:26 +0800

    fix: remove unnessary language file and revert other edits

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,6 @@
 import { i18nConfig } from "../../next-i18next.config.mjs";
 import en from "./en.json";
-import fr from "./fr.json";
-import ja from "./ja.json";
 import zh_Hans from "./zh-Hans.json";
-import zh_Hant from "./zh-Hant.json";
 
 type LocaleResources = {
   [key: string]: {
@@ -13,25 +10,13 @@ type LocaleResources = {
 };
 
 export const localeResources: LocaleResources = {
-  en: {
-    translation: en,
-    display_name: "English",
-  },
-  fr: {
-    translation: fr,
-    display_name: "Français",
-  },
-  ja: {
-    translation: ja,
-    display_name: "日本語",
-  },
   "zh-Hans": {
     translation: zh_Hans,
     display_name: "简体中文",
   },
-  "zh-Hant": {
-    translation: zh_Hant,
-    display_name: "繁體中文",
+  en: {
+    translation: en,
+    display_name: "English",
   },
 };
 
