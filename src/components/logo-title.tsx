b58e9fce1068f0ad0cbc0d0b6de7b930258eb4fd
a1375625918 <87858091+a1375625918@users.noreply.github.com>
2025-09-18 00:37:04 +0800

    fix: logo title localization

--- src/components/logo-title.tsx
+++ src/components/logo-title.tsx
@@ -1,25 +1,36 @@
 import { BoxProps, HStack, Heading, Highlight, Image } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
 import styles from "@/styles/logo-title.module.css";
 
 interface LogoTitleProps extends BoxProps {}
 
 export const TitleShort: React.FC<LogoTitleProps> = (props) => {
+  const { t } = useTranslation();
   return (
     <Heading size="md" className={styles.title} {...props}>
-      SUESMC
-      <Highlight query="C" styles={{ color: "blue.600", userSelect: "none" }}>
-        C
+      {t("LogoTitle.heading.short.text1")}
+      {t("LogoTitle.heading.short.space")}
+      <Highlight
+        query={t("LogoTitle.heading.short.query")}
+        styles={{ color: "blue.600", userSelect: "none" }}
+      >
+        {t("LogoTitle.heading.short.text2")}
       </Highlight>
     </Heading>
   );
 };
 
 export const TitleFull: React.FC<LogoTitleProps> = (props) => {
+  const { t } = useTranslation();
   return (
     <Heading size="md" className={styles.title} {...props}>
-      SUESMC{" "}
-      <Highlight query="C" styles={{ color: "blue.600", userSelect: "none" }}>
-        Client
+      {t("LogoTitle.heading.full.text1")}
+      {t("LogoTitle.heading.full.space")}
+      <Highlight
+        query={t("LogoTitle.heading.full.query")}
+        styles={{ color: "blue.600", userSelect: "none" }}
+      >
+        {t("LogoTitle.heading.full.text2")}
       </Highlight>
     </Heading>
   );
