d8951b876bfb5fa216c04aef1a4444b9a0bf2a4d
a1375625918 <87858091+a1375625918@users.noreply.github.com>
2025-11-16 00:16:15 +0800

    fix: update workflow script

--- scripts/release/generate_simple_changelog_draft.py
+++ scripts/release/generate_simple_changelog_draft.py
@@ -44,6 +44,7 @@ def main():
         return
     latest, previous = tags[0], tags[1]
     diff_url = f"https://github.com/{OWNER}/{REPO}/releases/tag/{latest}"
+    print(f"# {latest}\n")
     print(f"Synced upstream to {latest}, for more information please visit {diff_url}. \n\n---\n\n同步上游到 {latest} ，访问 {diff_url} 了解更多信息。")
 
 
